ef06e1b8e511c694b2dc54c1c1b9d02cb2a4520a
Xavier <luis.ochoa02@epn.edu.ec>
2025-11-26 03:37:43 -0500

    Add files via upload

---
+++ src/routers/veterinario_routes.js
@@ -0,0 +1,23 @@
+import {Router} from 'express'
+import { actualizarPassword, actualizarPerfil, comprobarTokenPasword, confirmarMail, crearNuevoPassword, login, perfil, 
+recuperarPassword, registro } from '../controllers/veterinario_controller.js'
+import { verificarTokenJWT } from '../middlewares/JWT.js'
+
+const router = Router()
+
+
+router.post('/registro',registro)
+router.get('/confirmar/:token',confirmarMail)
+
+router.post('/recuperarpassword',recuperarPassword)
+router.get('/recuperarpassword/:token',comprobarTokenPasword)
+router.post('/nuevopassword/:token',crearNuevoPassword)
+router.post('/veterinario/login',login) 
+
+router.get('/veterinario/perfil',verificarTokenJWT,perfil)
+
+router.put('/actualizarperfil/:id',verificarTokenJWT,actualizarPerfil)
+
+router.put('/actualizarpassword/:id',verificarTokenJWT,actualizarPassword)
+
+export default router
